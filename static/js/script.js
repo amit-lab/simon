f1898d47dbda4314d392517d23e15dffafd1461f
amit-lab <55277797+amit-lab@users.noreply.github.com>
2020-01-17 23:52:36 +0530

    simon game

---
+++ static/js/script.js
@@ -0,0 +1,68 @@
+
+// Variable decliration
+let buttonColor = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let levelNumber = 1;
+// let btnAudio = currentButton + ".mp3";
+
+
+// Function declaration
+
+function randomNumberGenerator() {
+    return Math.floor(Math.random() * 4);
+}
+
+function colorPicker() {
+    return buttonColor[randomNumberGenerator()];
+}
+
+
+$("body").keypress(function () {
+    run();
+});
+
+function createGamePattern(levelNo) {
+    setTimeout(function () {
+        let currentButton = colorPicker();
+        gamePattern.push(currentButton);
+        let currentAudio = new Audio("static/sounds/" + currentButton + ".mp3");
+        $("." + currentButton).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+        // currentAudio.play();
+        playSound(currentAudio);
+        if (levelNo !== 1) {
+            createGamePattern(levelNo - 1);
+        }
+    }, 500);
+}
+
+function playSound(currentAudio) {
+    currentAudio.play();
+}
+
+function run() {
+    $("h1").html("Level : " + levelNumber);
+    createGamePattern(levelNumber);
+}
+
+
+$(".btn").click(function (event) {
+    let current1 = event.currentTarget.classList;
+    let current2 = gamePattern.shift();
+    if (current2 !== current1[1]) {
+        let over = new Audio("static/sounds/wrong.mp3");
+        over.play();
+        $("h1").html("Game Over!!!<br>Press any key to restart the game!");
+        gameOver();
+    }
+    if (gamePattern.length === 0) {
+        levelNumber++;
+        run();
+    }
+});
+
+function gameOver() {
+    levelNumber = 1;
+    $("body").keypress(function () {
+        run();
+    });
+}
